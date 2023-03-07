ae8b752a99df942fc839a1e65a80b9d3a8b607b7
lemonbrain Marco <marco@lemonbrain.ch>
2023-03-07 14:14:43 +0100

    angefangen mit server stack, aktuelles problem im docker container kann *.graphql file nicht geladen werden

--- server/src/loader.js
+++ server/src/loader.js
@@ -3,6 +3,7 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { moviesResolvers } from './resolvers/movie.js'
 
 export const loadGraphQlSchema = async function () {
+    // todo: dieser pfad kann im docker container nicht gelesen werden?
   const typeDefs = await loadSchema('./schemas/**/*.graphql', {
     loaders: [new GraphQLFileLoader()],
   })
