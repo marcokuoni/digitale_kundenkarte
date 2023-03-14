1d2b6486e412444ec07e62a30eced8f119cd5f24
lemonbrain Marco <marco@lemonbrain.ch>
2023-03-14 11:22:33 +0100

    removed unused code added server url and possability to pass through env

--- client/src/apollo-client.ts
+++ client/src/apollo-client.ts
@@ -1,38 +1,10 @@
 import {
     ApolloClient,
     InMemoryCache,
-    HttpLink,
-    ApolloLink,
   } from '@apollo/client';
-  // import { getOperationAST } from 'graphql';
-  
-  // const cache = new InMemoryCache({
-  //   addTypename: true,
-  // });
-  
-  // const httpLink = new HttpLink({
-  //   uri: 'https://server.localhost:5001/graphql/',
-  // });
-  
-  // const link = ApolloLink.split(
-  //   (op: any) => {
-  //     // check if it is a subscription
-  //     const operationAST = getOperationAST(op.query, op.operationName);
-  //     return !!operationAST && operationAST.operation === 'subscription';
-  //   },
-  //   httpLink
-  // );
-  
-  // export default new ApolloClient({
-  //   cache,
-  //   link,
-  //   connectToDevTools: true,
-  // });
 
+  //!the process.env gets replaced by precompiler
   export default new ApolloClient({
-
-    uri: 'https://server.localhost:5001/graphql',
-  
-    cache: new InMemoryCache(),
-  
+    uri: `http://process.env.SERVER_URL/graphql`,  
+    cache: new InMemoryCache(),  
   });
