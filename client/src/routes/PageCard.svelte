8e95c9ad22c0a4c0ed0b01e1c0458696de7f0467
kevinloeffler <hi@kevinloeffler.ch>
2023-03-25 17:34:56 +0100

    ADD: bare-bones data fetching (not working)

--- client/src/routes/PageCard.svelte
+++ client/src/routes/PageCard.svelte
@@ -1,7 +1,44 @@
-<main>
-    <h1>{page}</h1>
-</main>
+<h1>Card Page</h1>
+
+<p>{ $dynUsers.data.getUsers }</p>
+<button on:click={logUsers}>Log users</button>
 
 <script lang="ts">
-    const page = 'Card Page'
+    /*
+    // this kinda works but does not use the codegen stuff
+
+    import {query, setClient} from 'svelte-apollo'
+    import gql from 'graphql-tag'
+    import client from '../apollo-client.js'
+
+    setClient(client)
+
+    const usersQueryDocument = gql`
+    query User {
+      getUsers {
+        _id
+        name
+      }
+    }
+    `
+
+    const users = query(usersQueryDocument)
+    console.log(users)
+
+    users.subscribe( (x) => {
+        console.log(x)
+        console.log('subscribed')
+    })
+    */
+
+    // does not return any users even if there are in the db
+
+    import { getUsers } from '../codegen.js'
+
+    $: dynUsers = getUsers( {} )
+
+    function logUsers() {
+        console.log($dynUsers)
+    }
+
 </script>
