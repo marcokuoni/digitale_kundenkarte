363e8ee80b7b9c667b4873f6ffee7e8f514f6eaa
kevinloeffler <hi@kevinloeffler.ch>
2023-03-28 22:59:47 +0200

    ADD: stamp component

---
+++ client/src/components/Stamp.svelte
@@ -0,0 +1,38 @@
+<div class="stamp" class:active-stamp={status}>
+    <img src="/images/CRN%20Logo%20Light.svg" class="stamp-image" class:hide-stamp-image={!status} alt="A white crown bar logo">
+</div>
+
+<script lang="ts">
+
+    export let status = false
+
+</script>
+
+<style>
+
+    .stamp {
+        width: 40px;
+        height: 40px;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+
+        border: 1px dashed var(--accent-color);
+        border-radius: 100%;
+    }
+
+    .active-stamp {
+        background-color: var(--accent-color);
+    }
+
+    .stamp-image {
+        width: 50%;
+        margin-top: -5%;
+    }
+
+    .hide-stamp-image {
+        display: none;
+    }
+
+</style>
