ed54ecfb6a2b60937537554fabfebcc763e02c04
lemonbrain Marco <marco@lemonbrain.ch>
2023-04-13 23:17:39 +0300

    logout/login done and tested, error for refresh prepared

---
+++ client/src/lib/apollo/client.ts
@@ -0,0 +1,39 @@
+import { ApolloClient, ApolloLink } from '@apollo/client'
+
+import OfflineLink from "./offline-link";
+import cache from "./cache";
+import httpLink from "./http-link";
+import errorLink from './error-link';
+import authLink from './auth-link';
+
+const offlineLink = new OfflineLink();
+
+const link = ApolloLink.from([
+  //NOTE deactivate local storage cache
+  offlineLink,
+  /// NOTE deactivate local storage cache
+  errorLink,
+  authLink,
+  httpLink,
+])
+
+
+
+const client = new ApolloClient({
+  link,
+  cache,
+  credentials: 'include',
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: "all",
+    },
+    query: {
+      errorPolicy: "all",
+    },
+    mutate: {
+      errorPolicy: "all"
+    }
+  }
+})
+
+export default client
